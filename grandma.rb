00382943fa6043c9c1d253812f1bc7f1f7ee9ddd
Christian Perez <perezchristian1012@gmail.com>
2019-07-22 17:03:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   phrase = "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
   end
-  if phrase = "Hi Nana, how are you?"
+  if phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   end
 
